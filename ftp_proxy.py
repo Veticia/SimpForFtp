fd5f333f1573cf09d722bde41270b25f43e9fb6b
Veticia <668573+Veticia@users.noreply.github.com>
2023-05-10 12:12:23 +0200

    sorting

--- ftp_proxy.py
+++ ftp_proxy.py
@@ -1,3 +1,4 @@
+import html
 import http.server
 import socketserver
 import ftplib
@@ -6,6 +7,7 @@ import urllib.parse
 from urllib.parse import unquote
 import ftpparser
 from datetime import datetime
+import mimetypes
 
 
 class FTPProxyHandler(http.server.BaseHTTPRequestHandler):
@@ -111,21 +113,35 @@ class FTPProxyHandler(http.server.BaseHTTPRequestHandler):
                     'path': item_path
                 })
 
+        name_link = generate_new_url(username, password, address, path)
+        size_link = '?SIZE_DESC'
+        date_link = '?DATE_DESC'
+        ext_link = '?EXT_ASC'
+
         # Sort the listing by name (case-insensitive) with directories first
         if sort_order == 'NAME_ASC':
+            name_link = '?NAME_DESC'
             listing.sort(key=lambda row: (row['type'] != 'directory', row['name'].lower()))
         elif sort_order == 'NAME_DESC':
             listing.sort(key=lambda row: (row['type'] != 'directory', row['name'].lower()), reverse=True)
         elif sort_order == 'SIZE_ASC':
             listing.sort(key=lambda row: (row['type'] != 'directory', row['size']))
         elif sort_order == 'SIZE_DESC':
+            size_link = '?SIZE_ASC'
             listing.sort(key=lambda row: (row['type'] != 'directory', row['size']), reverse=True)
         elif sort_order == 'DATE_ASC':
             listing.sort(key=lambda row: (row['type'] != 'directory', row['date']))
         elif sort_order == 'DATE_DESC':
+            date_link = '?DATE_ASC'
             listing.sort(key=lambda row: (row['type'] != 'directory', row['date']), reverse=True)
+        elif sort_order == 'EXT_ASC':
+            ext_link = '?EXT_DESC'
+            listing.sort(key=lambda row: (row['type'] != 'directory', os.path.splitext(row['name'])[1]))
+        elif sort_order == 'EXT_DESC':
+            listing.sort(key=lambda row: (row['type'] != 'directory', os.path.splitext(row['name'])[1]), reverse=True)
         else:
             # Default sort order
+            name_link = '?NAME_DESC'
             listing.sort(key=lambda row: (row['type'] != 'directory', row['name'].lower()))
 
         self.send_response(200)
@@ -133,7 +149,8 @@ class FTPProxyHandler(http.server.BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write('<html><head><title>FTP Proxy</title></head><body>'.encode())
         self.wfile.write('<table>'.encode())
-        self.wfile.write('<tr><th>Name</th><th>Size</th><th>Date</th></tr>'.encode())
+        self.wfile.write(
+            f'<tr><th><a href="{name_link}">Name</a></th><th><a href="{ext_link}">File type</a></th><th><a href="{size_link}">Size</a></th><th><a href="{date_link}">Date</a></th></tr>'.encode())
         if path != '':
             parent_path = os.path.dirname(path)
             parent_url = f'/proxy/{address}{parent_path}'.rstrip('/')
@@ -141,26 +158,27 @@ class FTPProxyHandler(http.server.BaseHTTPRequestHandler):
 
         for item in listing:
             size = ''
+            file_type = html.escape('<dir>')
             if item['type'] == 'file':
                 size = format_size(item["size"])
+                file_type = os.path.splitext(item["name"])[1].lstrip('.')
             date = datetime.fromtimestamp(item['date']).strftime('%Y-%m-%d %H:%M') if item['date'] else ''
-            if username and password:
-                new_url = f'/proxy/{username}:{password}@{address}{item["path"]}'
-            elif username:
-                new_url = f'/proxy/{username}@{address}{item["path"]}'
-            else:
-                new_url = f'/proxy/{address}{item["path"]}'
+            new_url = generate_new_url(username, password, address, item["path"])
             self.wfile.write(
-                f'<tr><td><a href="{new_url}">{item["name"]}</a></td><td>{size}</td><td>{date}</td></tr>'.encode())
+                f'<tr><td><a href="{new_url}">{item["name"]}</a></td><td>{file_type}</td><td>{size}</td><td>{date}</td></tr>'.encode())
 
         self.wfile.write('</table></body></html>'.encode())
 
     def handle_file_request(self, ftp, path):
         filename = os.path.basename(path)
+        mimetype, _ = mimetypes.guess_type(filename)
         filesize = ftp.size(path)  # Get the size of the file
         self.send_response(200)
-        self.send_header('Content-type', 'application/octet-stream')
-        self.send_header('Content-Disposition', f'attachment; filename="{filename}"')
+        if mimetype == 'application/octet-stream':
+            self.send_header('Content-type', 'application/octet-stream')
+            self.send_header('Content-Disposition', f'attachment; filename="{filename}"')
+        else:
+            self.send_header('Content-type', mimetype)
         self.send_header('Accept-Ranges',
                          'none')  # Server doesn't allow downloading from the middle of a file
         self.send_header('Content-Length', filesize)  # Send the size of the file
@@ -169,12 +187,24 @@ class FTPProxyHandler(http.server.BaseHTTPRequestHandler):
         ftp.retrbinary(f'RETR {path}', lambda data: self.wfile.write(data),
                        rest=None)  # Server doesn't allow retries
 
+
 def format_size(size):
     for unit in ['bytes', 'KB', 'MB', 'GB']:
         if size < 1024:
             return f"{size:.1f} {unit}"
         size /= 1024.0
     return f"{size:.1f} TB"
+
+
+def generate_new_url(self, password, address, path) -> str:
+    username = self
+    if username and password:
+        new_url = f'/proxy/{username}:{password}@{address}{path}'
+    elif username:
+        new_url = f'/proxy/{username}@{address}{path}'
+    else:
+        new_url = f'/proxy/{address}{path}'
+    return new_url
 
 
 def main():
